bc1a0e636083205ea6fda66ddcd3e121d5fa4217
Tats <js@drone.ws>
2004-07-27 05:06:29 +0000

    Smoothed the interface

--- src/SummedAreaTable.cpp
+++ src/SummedAreaTable.cpp
@@ -1,19 +1,14 @@
 #include "SummedAreaTable.h"
-#include <iostream>
-
-#include <assert.h>
 
 SummedAreaTable::SummedAreaTable()
-  : _acc(0), _accSquare(0), _canvas(0), _canvasData(0), _sizeX(0), _sizeY(0), _size(0)
+  : _acc(0), _accSquares(0), _canvas(0), _canvasData(0), _sizeX(0), _sizeY(0), _size(0)
 {
 }
 
 SummedAreaTable::~SummedAreaTable()
 {
-  if (_acc)
-    delete[] _acc;
-  if (_accSquare)
-    delete[] _accSquare;
+  free(_acc);
+  free(_accSquares);
 }
 
 void SummedAreaTable::setCanvas(Canvas *canvas)
@@ -25,8 +20,6 @@ void SummedAreaTable::setCanvas(Canvas *canvas)
     _sizeX = _canvas->sizeX();
     _sizeY = _canvas->sizeY();
     _size = _sizeX * _sizeY;
-
-    _acc = new RGBAint[_size];
   }
 }
 
@@ -36,19 +29,16 @@ void SummedAreaTable::buildTable()
     return;
   
   // check if we need to resize
-  if (_canvas->sizeX() * _canvas->sizeY() != _size) // there was a change...
+  if (_canvas->sizeX() * _canvas->sizeY() != _size) // there was a change
   {
     // resize
     _sizeX = _canvas->sizeX();
     _sizeY = _canvas->sizeY();
     _size = _sizeX * _sizeY;
-
-    // reallocate
-    // *** vérifier si on peut utiliser realloc ...
-    if (_acc)
-      delete[] _acc;
-    _acc = new RGBAint[_size];
   }
+
+  // reallocate
+  _acc = (RGBAint*)realloc(_acc, _size*sizeof(RGBAint));
 
   _canvasData = _canvas->_data;
   
@@ -60,7 +50,7 @@ void SummedAreaTable::buildTable()
   // upper row
   _accR = _accG = _accB = _accA = 0;
   for (int x=0; x<_sizeX; ++x)
-  {
+  { 
     _accR += (int)*(iterData++);
     _accG += (int)*(iterData++);
     _accB += (int)*(iterData++);
@@ -89,110 +79,63 @@ void SummedAreaTable::buildTable()
       *(iterAcc++) += _accA;
     }
   }
-
-//   for (int y=0; y<_sizeY; ++y)
-//   {
-//     for (int x=0; x<_sizeX; ++x)
-//     {
-//       RGBAint acc = _acc[y*_sizeX+x];
-//       RGBA val = _canvasData[y*_sizeX+x];
-// //       unsigned char r = _canvasData[y*_sizeX*4+x];
-// //       unsigned char g = _canvasData[y*_sizeX*4+x+1];
-// //       unsigned char b = _canvasData[y*_sizeX*4+x+1];
-// //       unsigned char a = _canvasData[y*_sizeX*4+x+1];
-//       printf("(%d %d %d %d)=",val.R, val.G, val.B, val.A);
-//       printf("(%d %d %d %d) ",acc.R, acc.G, acc.B, acc.A);
-//     }
-//     printf("\n");
-//   }
-
-//   for (int y=0; y<_sizeY; ++y)
-//   {
-//     for (int x=0; x<_sizeX; ++x)
-//       if (y==0)
-        
-//       if (_acc[y*_sizeX+x]
-//       }  
 }
 
-void SummedAreaTable::buildTable2()
+void SummedAreaTable::buildTableOfSquares()
 {
-
-}
-
-void SummedAreaTable::getSum(RGBAint *sum, int x0, int y0, int x1, int y1) const
-{
-  getSumIn(sum, _acc, x0, y0, x1, y1);
-}
-
-void SummedAreaTable::getSumSquare(RGBAint *sumSquare, int x0, int y0, int x1, int y1) const
-{
-  getSumIn(sumSquare, _accSquare, x0, y0, x1, y1);
-}
-
-int SummedAreaTable::getArea(int x0, int y0, int x1, int y1) const
-{
-  if (x0 < 0) x0 = 0;
-  if (x1 < 0) x1 = 0;
-  if (y0 < 0) y0 = 0;
-  if (y1 < 0) y1 = 0;
-  
-  if (x0 >= _sizeX) x0 = _sizeX-1;
-  if (x1 >= _sizeX) x1 = _sizeX-1;
-  if (y0 >= _sizeY) y0 = _sizeY-1;
-  if (y1 >= _sizeY) y1 = _sizeY-1;
-
-  return ((x1-x0) * (y1-y0));
-}
-
-
-void SummedAreaTable::getSumIn(RGBAint *sum, RGBAint *acc, int x0, int y0, int x1, int y1) const
-{
-  if (!sum)
+  if (!_canvas)
     return;
-
-  memset(sum, 0, sizeof(RGBAint)); // reset
   
-  // it is assumed that (x0,y0) <= (x1,y1)
+  // check if we need to resize
+  if (_canvas->sizeX() * _canvas->sizeY() != _size) // there was a change
+  {
+    // resize
+    _sizeX = _canvas->sizeX();
+    _sizeY = _canvas->sizeY();
+    _size = _sizeX * _sizeY;
+  }
 
-  // check upper bounds
-  if (x0 >= _sizeX) x0 = _sizeX-1;
-  if (x1 >= _sizeX) x1 = _sizeX-1;
-  if (y0 >= _sizeY) y0 = _sizeY-1;
-  if (y1 >= _sizeY) y1 = _sizeY-1;
+  // reallocate
+  _accSquares = (RGBAint*)realloc(_accSquares, _size*sizeof(RGBAint));
 
-  if (x0 < 0) x0 = 0;
-  if (x1 < 0) x1 = 0;
-  if (y0 < 0) y0 = 0;
-  if (x1 < 0) y1 = 0;
-
-  addRGBAint(sum, &acc[y1 * _sizeX + x1]);
-  subRGBAint(sum, &acc[y1 * _sizeX + x0]);
-  subRGBAint(sum, &acc[y0 * _sizeX + x1]);
-  addRGBAint(sum, &acc[y0 * _sizeX + x0]);
+  _canvasData = _canvas->_data;
   
-//   if (! (sum->R >= 0 && sum->G >= 0 && sum->B >=0 && sum->A >= 0 ) )
-//   {
-//     std::cout << "[" << _sizeX << "," << _sizeY << "] (" << x0 << "," << y0 << ") (" << x1 << "," << y1 << ")" << std::endl;
-//   }
-      
-//   // calculate it efficiently
-//   if (x0 < 0)
-//   {
-//     if (y0 >= 0)
-//       subRGBAint(sum, &acc[y0 * _sizeX + x1]);
-//   }
-//   else if (y0 < 0)
-//   {
-//     subRGBAint(sum, &acc[y1 * _sizeX + x0]);
-//   }
-//   else
-//   {
-//     addRGBAint(sum, &acc[y0 * _sizeX + x0]);
-//     subRGBAint(sum, &acc[y1 * _sizeX + x0]);
-//     subRGBAint(sum, &acc[y0 * _sizeX + x1]);
-//   }
+  unsigned char *iterData = (unsigned char*) _canvasData;
+  int *iterAcc = (int*) _acc;
 
-//  addRGBAint(sum, &acc[y1 * _sizeX + x1]);
+  int rowWidth = _sizeX * 4;
   
+  // upper row
+  _accR = _accG = _accB = _accA = 0;
+  for (int x=0; x<_sizeX; ++x)
+  {
+    _accR += square( (int)*(iterData++) );
+    _accG += square( (int)*(iterData++) );
+    _accB += square( (int)*(iterData++) );
+    _accA += square( (int)*(iterData++) );
+    *(iterAcc++) = _accR;
+    *(iterAcc++) = _accG;
+    *(iterAcc++) = _accB;
+    *(iterAcc++) = _accA;
+  }
+
+  // other rows
+  for (int y=1; y<_sizeY; ++y)
+  {
+    // copy upper line
+    memcpy(iterAcc, iterAcc - rowWidth, rowWidth * sizeof(int));
+    _accR = _accG = _accB = _accA = 0;
+    for (int x=0; x<_sizeX; ++x)
+    {
+      _accR += square( (int)*(iterData++) );
+      _accG += square( (int)*(iterData++) );
+      _accB += square( (int)*(iterData++) );
+      _accA += square( (int)*(iterData++) );
+      *(iterAcc++) += _accR;
+      *(iterAcc++) += _accG;
+      *(iterAcc++) += _accB;
+      *(iterAcc++) += _accA;
+    }
+  }
 }
+
--- src/SummedAreaTable.h
+++ src/SummedAreaTable.h
@@ -2,6 +2,11 @@
 #define SUMMEDAREATABLE_INCLUDED
 
 #include "Canvas.h"
+#include "Utils.h"
+
+#include <iostream>
+
+#include <assert.h>
 
 class SummedAreaTable
 {
@@ -13,48 +18,27 @@ public:
   void setCanvas(Canvas *canvas);
 
   void buildTable();
-  void buildTable2();
+  void buildTableOfSquares();
 
-  void getSum(RGBAint *sum, int x0, int y0, int x1, int y1) const;
-  void getSumSquare(RGBAint *sumSquare, int x0, int y0, int x1, int y1) const;
-  int getArea(int x0, int y0, int x1, int y1) const;
+  // *** il le faut pour les grayscales aussi...
+
+  inline void getSum(RGBAint *sum, int x0, int y0, int x1, int y1) const;
+  inline void getSumOfSquares(RGBAint *sumSquares, int x0, int y0, int x1, int y1) const;
+
+  inline RGBAint *getAcc(int x, int y) const;
+  inline RGBAint *getAccOfSquares(int x, int y) const;
+  
+  inline int getArea(int x0, int y0, int x1, int y1) const;
 
   static inline int total(RGBAint *sum)
   {
     return sum->R + sum->G + sum->B;
-//     int ret = 0;
-//     int *sumIter = (int*)sum;
-//     for (int i=0; i<4; ++i)
-//       ret += *(sumIter++);
-//     return ret;
   }
-
-private:
-  void getSumIn(RGBAint *sum, RGBAint *acc, int x0, int y0, int x1, int y1) const;
-
-  static inline void addRGBAint(RGBAint *val, RGBAint *inc)
-  {
-    int *valIter = (int*)val;
-    int *incIter = (int*)inc;
-    for (int i=0; i<4; ++i)
-      *(valIter++) += *(incIter++);
-  }
-
-  static inline void subRGBAint(RGBAint *val, RGBAint *inc)
-  {
-    int *valIter = (int*)val;
-    int *incIter = (int*)inc;
-    for (int i=0; i<4; ++i)
-      *(valIter++) -= *(incIter++);
-  }
-
 public:
   
   RGBAint *_acc;
-  //int *_accGray;
 
-  RGBAint *_accSquare;
-  //int *_accGray2;
+  RGBAint *_accSquares;
 
   Canvas *_canvas;
 
@@ -65,5 +49,78 @@ public:
   int _accR, _accG, _accB, _accA;
   
 };
+
+void SummedAreaTable::getSum(RGBAint *sum, int x0, int y0, int x1, int y1) const
+{
+  if (!sum)
+    return;
+
+  memset(sum, 0, sizeof(RGBAint)); // reset
+  
+  // it is assumed that (x0,y0) <= (x1,y1)
+
+  *sum += _acc[y1 * _sizeX + x1];
+  
+  if (x0 >= 0)
+    *sum -= _acc[y1 * _sizeX + x0];
+  if (y0 >= 0)
+  {
+    *sum -= _acc[y0 * _sizeX + x1];
+    if (x0 >= 0)
+      *sum += _acc[y0 * _sizeX + x0];
+  }
+}
+
+void SummedAreaTable::getSumOfSquares(RGBAint *sumSquares, int x0, int y0, int x1, int y1) const
+{
+  if (!sumSquares)
+    return;
+
+  memset(sumSquares, 0, sizeof(RGBAint)); // reset
+  
+  // it is assumed that (x0,y0) <= (x1,y1)
+
+  *sumSquares += _accSquares[y1 * _sizeX + x1];
+  
+  if (x0 >= 0)
+    *sumSquares -= _accSquares[y1 * _sizeX + x0];
+  if (y0 >= 0)
+  {
+    *sumSquares -= _accSquares[y0 * _sizeX + x1];
+    if (x0 >= 0)
+      *sumSquares += _accSquares[y0 * _sizeX + x0];
+  }
+}
+
+int SummedAreaTable::getArea(int x0, int y0, int x1, int y1) const
+{
+  if (x0 < 0) x0 = 0;
+  if (x1 < 0) x1 = 0;
+  if (y0 < 0) y0 = 0;
+  if (y1 < 0) y1 = 0;
+  
+  if (x0 >= _sizeX) x0 = _sizeX-1;
+  if (x1 >= _sizeX) x1 = _sizeX-1;
+  if (y0 >= _sizeY) y0 = _sizeY-1;
+  if (y1 >= _sizeY) y1 = _sizeY-1;
+
+  return ((x1-x0) * (y1-y0));
+}
+
+RGBAint *SummedAreaTable::getAcc(int x, int y) const
+{
+  if (x < 0 || y < 0)
+    return 0;
+  else
+    return &_acc[y * _sizeX + x];
+}
+
+RGBAint *SummedAreaTable::getAccOfSquares(int x, int y) const
+{
+  if (x < 0 || y < 0)
+    return 0;
+  else
+    return &_accSquares[y * _sizeX + x];
+}
 
 #endif
