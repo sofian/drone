28b9c760901b8661027165e783ddd1a99d89e705
Tats <js@drone.ws>
2004-09-02 05:01:48 +0000

    Reindented.

--- src/Gear_VideoAdd.h
+++ src/Gear_VideoAdd.h
@@ -9,48 +9,48 @@ class Gear_VideoAdd : public Gear
 {
 public:
 
-    Gear_VideoAdd(Engine *engine, std::string name);
-    virtual ~Gear_VideoAdd();
+  Gear_VideoAdd(Engine *engine, std::string name);
+  virtual ~Gear_VideoAdd();
     
-    void runVideo();
+  void runVideo();
 
-    PlugVideoIn* VIDEO_IN_A(){return _VIDEO_IN_A;};
-    PlugVideoIn* VIDEO_IN_B(){return _VIDEO_IN_B;};
-    PlugVideoOut* VIDEO_OUT(){return _VIDEO_OUT;};
+  PlugVideoIn* VIDEO_IN_A(){return _VIDEO_IN_A;};
+  PlugVideoIn* VIDEO_IN_B(){return _VIDEO_IN_B;};
+  PlugVideoOut* VIDEO_OUT(){return _VIDEO_OUT;};
     
-    bool ready();
+  bool ready();
 
 private:
 
 	PlugVideoIn *_VIDEO_IN_A;
-    PlugVideoIn *_VIDEO_IN_B;
+  PlugVideoIn *_VIDEO_IN_B;
 	PlugVideoOut *_VIDEO_OUT;
 
-    //local var
-    Video_T *_imageA; 
-    Video_T *_imageB; 
-    Video_T *_outImage; 
-    RGBA *_dataA;
-    RGBA *_dataB;
-    RGBA *_outData;
-    RGBA *_largerImage;
-    RGBA *_tallerImage;
+  //local var
+  Video_T *_imageA; 
+  Video_T *_imageB; 
+  Video_T *_outImage; 
+  RGBA *_dataA;
+  RGBA *_dataB;
+  RGBA *_outData;
+  RGBA *_largerImage;
+  RGBA *_tallerImage;
 
-    int _iterSizeY;
-    int _iterSizeX;    
-    int _imageASizeX;
-    int _imageASizeY;
-    int _imageBSizeX;
-    int _imageBSizeY;
-    int _imageOutSizeX;
-    int _imageOutSizeY;
+  int _iterSizeY;
+  int _iterSizeX;    
+  int _imageASizeX;
+  int _imageASizeY;
+  int _imageBSizeX;
+  int _imageBSizeY;
+  int _imageOutSizeX;
+  int _imageOutSizeY;
         
-    double *_mmxImageInA;
-    double *_mmxImageInB;
-    double *_mmxImageOut;
+  double *_mmxImageInA;
+  double *_mmxImageInB;
+  double *_mmxImageOut;
 
-    int _remainIterSizeY;
-    int _remainIterSizeX;
+  int _remainIterSizeY;
+  int _remainIterSizeX;
     
 };
 
