17ef3a7b13e7cb06e496f961743018f7216ec2d3
Tats <js@drone.ws>
2004-09-09 17:12:20 +0000

    Un-subclassed MatrixType from std::vector, because it was doing a useless and costfull fill upon calling.

--- src/types/MatrixType.h
+++ src/types/MatrixType.h
@@ -4,17 +4,17 @@
 #include "ValueType.h"
 
 template <class T>
-class MatrixType : public AbstractType, public std::vector<T>
+class MatrixType : public AbstractType
 {
 public:
   MatrixType(int width = 0,
              int height = 0, 
              T fillValue = T())
-    : _width(width),
-      _height(height)
+    : _data(0)
     {
       addSubType(_width);
       addSubType(_height);
+      resize(width, height);
       fill(fillValue);
     }
   
@@ -23,43 +23,51 @@ public:
   const ValueType& width() const { return _width; }
   const ValueType& height() const { return _height; }
 
-  int sizeX() const { return static_cast<int>(_width); }
-  int sizeY() const { return static_cast<int>(_height); }
+  int size() const { return _size; }
+  int sizeX() const { return _sizeX; }
+  int sizeY() const { return _sizeY; }
+
+  T& operator[](int i) const { return _data[i]; }
+  T operator()(int x, int y) const { return _data[y*_sizeX+x]; }
   
-  T operator()(int x, int y) const { return operator[](x*static_cast<int>(_width)+y); }
-  
-  T* data() { return &operator[](0); }
+  T* data() { return _data; }
   
   virtual std::string name() const {return "!error! UndefinedMatrixType";} 
   virtual QColor color() const {  return QColor(127,127,127); }
   
-  void resize(size_t size)
-  {
-    std::vector<T>::resize(size);
-  }
-
-  void resize(int width, int height) 
-  {
-    _height.setValue(height);
-    _width.setValue(width);
-    resize(width*height);
-  }
-  
   void resize(const ValueType& width, const ValueType &height) 
   {
-    _width = width;
-    _height = height;
-    resize((int)(height.value()*width.value()));
+    resize(width.value(), height.value());
   }
 
+  void resize(int sizeX, int sizeY=1) 
+  {
+    _height.setValue(sizeX);
+    _width.setValue(sizeY);
+    _sizeX = sizeX;
+    _sizeY = sizeY;
+    allocate(sizeX*sizeY);
+  }
+  
   void fill(T value)
   {
-    std::fill(begin(), end(), value);
+    for (T *it = _data; it != _data+_size; ++it)
+      *it++ = value;
   }
 
 private:
+  void allocate(size_t size)
+  {
+    _data = (T*)realloc(_data, size*sizeof(T));
+    _size = size;
+  }
+
   ValueType _width;
   ValueType _height;
+  int _size;
+  int _sizeX;
+  int _sizeY;
+  T *_data;
 };
 
 /* template<>                                   */
