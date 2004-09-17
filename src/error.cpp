5b69921810d9cfab9e2a30056d29a742d6526ea1
Tats <js@drone.ws>
2004-09-17 17:56:25 +0000

    Debugging module

---
+++ src/error.cpp
@@ -0,0 +1,57 @@
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+char xxpetit_message_pour_melanie[10000];
+
+void errormsg(const char* msg, ...)
+{
+  va_list args;
+  va_start(args,msg);
+  vsprintf(xxpetit_message_pour_melanie, msg, args);
+  printf("\n$ Error: %s\n\n", xxpetit_message_pour_melanie);
+  fflush(stdout);
+  va_end(args);
+  exit(-1);
+}
+
+void warningmsg(const char* msg, ...)
+{
+  va_list args;
+  va_start(args,msg);
+  vsprintf(xxpetit_message_pour_melanie, msg, args);
+  printf("! Warning: %s\n", xxpetit_message_pour_melanie);
+  fflush(stdout);
+  va_end(args);
+}
+
+void noticemsg(const char* msg, ...)
+{
+  va_list args;
+  va_start(args,msg);
+  vsprintf(xxpetit_message_pour_melanie, msg, args);
+  printf("! Notice: %s\n",  xxpetit_message_pour_melanie);
+  fflush(stdout);
+  va_end(args);
+}
+
+void message(const char* msg, ...)
+{
+  va_list args;
+  va_start(args,msg);
+  vsprintf(xxpetit_message_pour_melanie, msg, args);
+  printf("# %s\n", xxpetit_message_pour_melanie);
+  fflush(stdout);
+  va_end(args);
+}
+
+void print(const char* msg, ...)
+{
+  va_list args;
+  va_start(args,msg);
+  vsprintf(xxpetit_message_pour_melanie, msg, args);
+  printf("%s", xxpetit_message_pour_melanie);
+  fflush(stdout);
+  va_end(args);
+}
---
+++ src/error.h
@@ -0,0 +1,53 @@
+#ifndef ERROR_INCLUDED
+#define ERROR_INCLUDED
+
+#define LEVEL_ERROR   0
+#define LEVEL_WARNING 1
+#define LEVEL_NOTICE  2
+
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL LEVEL_ERROR
+#endif
+
+// This macro is absolutely NOT SAFE! NEVER USE IT ALONE!!!
+// Use ASSERT_ERROR, ASSERT_WARNING and ASSERT_NOTICE instead, below.
+#define TRIGGER_ASSERT(expr, func) \
+((expr) ? static_cast<void>(0) : func(__STRING(expr)) );
+
+#define ASSERT_ERROR(expr)   TRIGGER_ASSERT(expr, ERROR)
+#define ASSERT_WARNING(expr) TRIGGER_ASSERT(expr, WARNING)
+#define ASSERT_NOTICE(expr)  TRIGGER_ASSERT(expr, NOTICE)
+
+inline void ERROR(const char* msg, ...);
+inline void WARNING(const char* msg, ...);
+inline void NOTICE(const char* msg, ...);
+
+void message(const char* msg, ...);
+void print(const char* msg, ...);
+
+void errormsg(const char* msg, ...);
+void warningmsg(const char* msg, ...);
+void noticemsg(const char* msg, ...);
+
+void ERROR(const char* msg, ...)
+{
+#if DEBUG_LEVEL >= LEVEL_ERROR
+  errormsg(msg);
+#endif
+}
+
+void WARNING(const char* msg, ...)
+{
+#if DEBUG_LEVEL >= LEVEL_WARNING
+  warningmsg(msg);
+#endif
+}
+
+void NOTICE(const char* msg, ...)
+{
+#if DEBUG_LEVEL >= LEVEL_NOTICE
+  noticemsg(msg);
+#endif
+}
+
+#endif
