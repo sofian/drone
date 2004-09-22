01388b385e15e1746a7f8669993a19e3389558c0
Tats <js@drone.ws>
2004-09-22 03:18:34 +0000

    Fixed bug dummy() should be dummymsg()

--- src/error.h
+++ src/error.h
@@ -81,7 +81,7 @@
 #endif
 
 // Dummy method (used for empty macros, see up there).
-inline void dummy(const char* msg, ...) {}
+inline void dummymsg(const char* msg, ...) {}
 
 //! Prints a message.
 void message(const char* msg, ...);
