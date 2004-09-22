0dffb1340828f3b7953062237ba12f0cfc978d2d
Tats <js@drone.ws>
2004-09-22 02:17:50 +0000

    Fully commented.

--- src/Matrix.h
+++ src/Matrix.h
@@ -3,10 +3,18 @@
 
 #include <vector>
 
+/**
+ * This class is a general-purpose structure for accessing data structured as
+ * a two-dimensional array. The memory it contains is contiguous as the class
+ * derives from the STL <code>vector</code> class. The main advantage of using
+ * a <code>Matrix</code> over a <code>vector</code> is that it offers a nice
+ * interface for accessing data in a "by-row" fashion.
+ */
 template <class T>
 class Matrix : public std::vector<T>
 {
 public:
+  //! Inner type definitions.
   typedef typename std::vector<T>::size_type size_type;
   typedef typename std::vector<T>::iterator iterator;
   typedef typename std::vector<T>::const_iterator const_iterator;
@@ -14,38 +22,103 @@ public:
   typedef typename std::vector<T>::pointer pointer;
 
 public:
+  /**
+   * Default constructor.
+   */
   Matrix()
   {
     resize(0,0);
   }
 
+  /**
+   * Constructor.
+   *
+   * @param width the wished number of columns
+   * @param height the wished number of rows
+   */
   Matrix(size_type width,
          size_type height)
   {
     resize(width, height);
   }
   virtual ~Matrix() {}
-  
+
+  /**
+   * Returns the number of rows.
+   *
+   * @return the height
+   */
   size_type height() const { return _height; }
+
+  /**
+   * Returns the number of columns.
+   *
+   * @return the width
+   */
   size_type width() const { return _width; }
 
-  reference operator()(size_type x, size_type y) const { return _rows[y][x]; }
-  reference get(size_type x, size_type y)  const { return _rows[y][x]; }
-  
+  /**
+   * Returns a reference to the value at coordinates <code>(x,y)</code>.
+   *
+   * @param x the column index
+   * @param y the row index
+   * @return the value
+   */
+  reference operator()(size_type x, size_type y) const
+  {
+    ASSERT_ERROR(0 <= x && x < width());
+    ASSERT_ERROR(0 <= y && y < height());
+    return _rows[y][x];
+  }
+
+  /**
+   * Returns a reference to the value at coordinates <code>(x,y)</code>.
+   *
+   * @param x the column index
+   * @param y the row index
+   * @return the value
+   */
+  reference get(size_type x, size_type y) const
+  {
+    ASSERT_ERROR(x < width());
+    ASSERT_ERROR(y < height());
+    return _rows[y][x];
+  }
+
+  /**
+   * Returns a reference to the vector of rows.
+   *
+   * @return the reference to the row vector
+   */
   std::vector<pointer>& rows() const { return _rows; }
-  
+
+  /**
+   * Resizes the matrix as a vector. Calling this sets <code>height</code> to <code>1</code>
+   * and <code>width</code> to <code>size</code>.
+   *
+   * @param size the new size
+   */
   void resize(size_type size)
   {
     std::vector<T>::resize(size);
     _width=size;
     _height=1;
+
+    _rows.resize(1);
+    _rows[0] = &front();
   }
 
+  /**
+   * Resizes the matrix to new width/height.
+   *
+   * @param width the new width
+   * @param height the new height
+   */
   void resize(size_type width, size_type height)
   {
     if (width==_width && height==_height)
       return;
-      
+    
     std::vector<T>::resize(width*height);
     _width = width;
     _height = height;
@@ -58,11 +131,38 @@ public:
       *it = iterData;
   }               
 
-  const pointer row(int i) const { return _rows[i]; }
-  pointer row(int i) { return _rows[i]; }
+  /**
+   * Returns a pointer to the first element of a row (const version).
+   *
+   * @param y the row number
+   * @return a pointer to the start of the row
+   */
+  const pointer row(size_type y) const
+  {
+    ASSERT_ERROR(y < height());
+    return _rows[y];
+  }
+
+  /**
+   * Returns a pointer to the first element of a row.
+   *
+   * @param y the row number
+   * @return a pointer to the start of the row
+   */
+  pointer row(size_type y)
+  {
+    ASSERT_ERROR(y < height());
+    return _rows[y];
+  }
 
 protected:
-  size_type _width, _height;
+  //! The number of columns.
+  size_type _width;
+
+  //! The number of rows.
+  size_type _height;
+
+  //! The precomputed array of row pointers.
   std::vector<pointer> _rows;
 };
 
