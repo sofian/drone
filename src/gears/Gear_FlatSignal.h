ba18018b85f04f8ed53b946a4e48ecea5870cf20
Tats <js@drone.ws>
2004-09-30 03:07:32 +0000

    Commented

--- src/gears/Gear_FlatSignal.h
+++ src/gears/Gear_FlatSignal.h
@@ -5,10 +5,20 @@
 #include "types/SignalType.h"
 #include "types/ValueType.h"
 
+/**
+ * Generates a flat signal, that is, a signal with a constant value. Can be used more
+ * generally as a converter from <code>ValueType</code> to <code>SignalType</code>.
+ * 
+ * @author Jean-Sébastien Senécal
+ * @version %I% %G%
+ */
 class Gear_FlatSignal : public GearConverter<ValueType, SignalType>
 {
 public:
+  //! Default constructor.
   Gear_FlatSignal(Engine *engine, std::string name);
+
+  // Destructor.
   virtual ~Gear_FlatSignal() {}
 
   void convert();
