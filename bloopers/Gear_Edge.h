d596099c20bc68649b2e6934ab9140a2b7cb4498
Tats <js@drone.ws>
2004-10-02 03:58:17 +0000

    Buggy gears that are cool

---
+++ bloopers/Gear_Edge.h
@@ -0,0 +1,55 @@
+#ifndef GEAR_EDGE_INCLUDED
+#define GEAR_EDGE_INCLUDED
+
+
+#include "Gear.h"
+#include "SignalType.h"
+#include "VideoType.h"
+
+
+class Gear_EdgeBlooper : public Gear
+{
+public:
+
+  Gear_EdgeBlooper(Engine *engine, std::string name);
+  virtual ~Gear_EdgeBlooper();
+
+  void runVideo();
+  bool ready();
+
+private:
+
+  PlugIn<VideoTypeRGBA> *_VIDEO_IN;
+  PlugOut<VideoTypeRGBA> *_VIDEO_OUT;
+  PlugIn<ValueType> *_AMOUNT_IN;
+
+  //local var
+  const VideoTypeRGBA *_image; 
+  VideoTypeRGBA *_outImage; 
+  unsigned char *_data;
+  unsigned char *_outData;
+
+  int _sizeY;
+  int _sizeX;    
+
+  RGBA *_iterPrevRow;
+  RGBA *_iterCurrRow;
+  RGBA *_iterNextRow;
+  RGBA *_iterOutData;
+
+  //  RGBA _adjust[2];
+
+//   double *_mmxImageInL0;
+//   double *_mmxImageInL02;
+//   double *_mmxImageInL1;
+//   double *_mmxImageInL12;
+//   double *_mmxImageInL2;
+//   double *_mmxImageInL22;
+//   double *_mmxImageOut;    
+//   double *_mmxAdjust;
+
+
+
+};
+
+#endif
