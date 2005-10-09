b9edb54185f3e8f7271e94e7f9881f58a4c0870e
Tats <js@drone.ws>
2005-10-09 23:35:37 +0000

    Templatized

--- src/exp/piksel05/SimpleGraph.h
+++ src/exp/piksel05/SimpleGraph.h
@@ -4,24 +4,42 @@
 #include "error.h"
 #include "Array.h"
 
-struct SimpleVertex
-{
-  // va contenir les informations sur un point
-  float x, y; // position
-  void *data;
-};
-
-class SimpleGraph : Array<SimpleVertex>
+template <class T>
+class SimpleGraph : Array<T>
 {
 public:
-  SimpleGraph(size_type n = 0);
+  SimpleGraph(size_type n = 0) : Array<T>(n)
+  {
+    edges.resize(n,n);
+    edges.fill(false);
+  }
+  
   ~SimpleGraph() {}
 
-  void clear();
-  void resize(size_type n, SimpleVertex t = SimpleVertex());
-  void addEdge(int i, int j, bool twoWays = true);
-  bool hasEdge(int i, int j);
+  void clear()
+  {
+    Array<T>::clear();
+    edges.clear();
+  }
+
+  void resize(size_type n, T t = T());
+  {
+    Array<T>::resize(n, t);
+    edges.resize(n, n);
+    edges.fill(false);
+  }
+  
+  void addEdge(int i, int j, bool twoWays = true)
+  {
+    edges(i,j) = true;
+    if (twoWays)
+      edges(j,i) = true;
+  }
+  
+  bool hasEdge(int i, int j) { return edges(i,j); }
   
 protected:
   Array2D<bool> edges;
 };
+
+#endif
