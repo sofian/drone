8bc27539cc82ca389d716952f5198f8f22e6ec03
Tats <js@drone.ws>
2005-10-14 16:37:31 +0000

    Fix: public Array

--- src/exp/piksel05/SimpleGraph.h
+++ src/exp/piksel05/SimpleGraph.h
@@ -6,9 +6,24 @@
 #include "Array2D.h"
 
 template <class T>
-class SimpleGraph : Array<T>
+class SimpleGraph : public Array<T>
 {
 public:
+  //! Inner type definitions.
+  typedef typename std::vector<T>::size_type size_type;
+  typedef typename std::vector<T>::iterator iterator;
+  typedef typename std::vector<T>::const_iterator const_iterator;
+  typedef typename std::vector<T>::reference reference;
+  typedef typename std::vector<T>::const_reference const_reference;
+  typedef typename std::vector<T>::pointer pointer;
+
+  
+public:
+  // using (as suggested by Norm)
+  using std::vector<T>::end;
+  using std::vector<T>::begin;
+  using std::vector<T>::front;
+
   SimpleGraph(size_type n = 0) : Array<T>(n)
   {
     _edges.resize(n,n);
@@ -23,7 +38,7 @@ public:
     _edges.clear();
   }
 
-  void resize(size_type n, T t = T());
+  void resize(size_type n, T t = T())
   {
     Array<T>::resize(n, t);
     _edges.resize(n, n);
