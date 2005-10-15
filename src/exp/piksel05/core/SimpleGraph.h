ae0357af924038140191651194d3828a04f77b16
Tats <js@drone.ws>
2005-10-15 15:16:21 +0000

    added function neighbors

--- src/exp/piksel05/core/SimpleGraph.h
+++ src/exp/piksel05/core/SimpleGraph.h
@@ -11,7 +11,21 @@ class SimpleGraph : public std::map<K, T>
 {
   typedef std::set<std::pair<K, K> > SimpleEdges;
   typedef std::map<K, T> parent;
-  
+
+public:
+  //! Inner type definitions.
+  typedef typename parent::size_type size_type;
+  typedef typename parent::iterator iterator;
+  typedef typename parent::const_iterator const_iterator;
+  typedef typename parent::reference reference;
+  typedef typename parent::const_reference const_reference;
+  typedef typename parent::pointer pointer;
+
+public:
+  // using (as suggested by Norm)
+  using parent::end;
+  using parent::begin;
+
 public:
   SimpleGraph() : parent() {}
   
@@ -36,13 +50,22 @@ public:
       _edges.insert(std::make_pair(to, from));
   }
   
-  bool hasEdge(const K& from, const K& to)
+  bool hasEdge(const K& from, const K& to) const
   {
     // Check that they exist.
     ASSERT_ERROR( find(from) != end() );
     ASSERT_ERROR( find(to) != end() );
 
     return (_edges.find(std::make_pair(from, to)) != _edges.end());
+  }
+
+  std::set<K> neighbors(const K& e) const
+  {
+    std::set<K> neighbors;
+    for (const_iterator it = begin(); it != end(); ++it)
+      if (hasEdge(e, it->first))
+        neighbors.insert(it->first);
+    return neighbors;
   }
   
 protected:
