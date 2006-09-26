278fd346f2876f3a8e818f265013bfb4ebeaf841
Tats <js@drone.ws>
2006-09-26 03:51:30 +0000

    Create a simple drone app with splash screen using JPF.

---
+++ plugins/drone.core/src/drone/core/CorePlugin.java
@@ -0,0 +1,37 @@
+package drone.core;
+
+import org.java.plugin.boot.Application;
+import org.java.plugin.boot.ApplicationPlugin;
+import org.java.plugin.util.ExtendedProperties;
+
+public final class CorePlugin extends ApplicationPlugin implements Application {
+    /**
+     * This plug-in ID.
+     */
+    public static final String PLUGIN_ID = "drone.core";
+
+	@Override
+	protected org.java.plugin.boot.Application initApplication(
+			ExtendedProperties arg0, String[] arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	@Override
+	protected void doStart() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("START");
+	}
+
+	@Override
+	protected void doStop() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("STOP");
+	}
+
+	public void startApplication() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("START APP");
+	}
+
+}
