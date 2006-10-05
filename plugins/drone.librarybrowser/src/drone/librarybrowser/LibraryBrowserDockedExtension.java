969ccbb84f0659f199a804bb88bd037a903c4837
Foogy <foogy@videotron.ca>
2006-10-05 22:01:47 +0000

    holy crap!

---
+++ plugins/drone.librarybrowser/src/drone/librarybrowser/LibraryBrowserDockedExtension.java
@@ -0,0 +1,32 @@
+package drone.librarybrowser;
+
+import javax.swing.JComponent;
+import javax.swing.JTree;
+
+import ptolemy.kernel.CompositeEntity;
+
+import net.infonode.docking.View;
+import drone.core.Configuration;
+import drone.core.extensions.DockedExtension;
+import drone.core.extensions.FailToCreateComponentException;
+
+public class LibraryBrowserDockedExtension implements DockedExtension {
+
+	public Position position() {
+		return Position.UPPER_LEFT;
+	}
+
+	public JComponent createComponent() throws FailToCreateComponentException {
+		// Create the tree view of the actor library.
+		// Open the configuration.
+
+		Configuration configuration = (Configuration) Configuration.configurations().iterator().next();
+		if (configuration == null) {
+			throw new FailToCreateComponentException("There are no existing configurations.");
+		}
+
+		CompositeEntity actorList = (CompositeEntity) configuration.getEntity("actor library");
+		return new EntityTree(actorList);
+	}
+
+}
