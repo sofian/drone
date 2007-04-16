e4f4d4f29b712404d8286c0e0f372b218a2b34f3
Tats <js@drone.ws>
2007-04-16 11:32:52 +0000

    Added GPL headers.

--- plugins/drone.artkp/src/drone/artkp/jni/Tracker.java
+++ plugins/drone.artkp/src/drone/artkp/jni/Tracker.java
@@ -1,12 +1,28 @@
-package ptolemy.actor.lib.artkp.kernel;
+/* The abstract Tracker superclass.
+ *  
+ * Copyright (c) 2006 Jean-Sebastien Senecal (js@drone.ws)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package drone.artkp.jni;
 
 public abstract class Tracker {
 
 	protected Tracker() {
-//		System.out.println("creating tracker handle");
 		_trackerHandle = _createTrackerHandle();
 		_initTracker();
-//		System.out.println("tracker handle = " + _trackerHandle);
 		if (_trackerHandle == 0)
 			throw new InternalError("Created handle is null, cannot proceed.");
 	}
