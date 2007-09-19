d74b7ac47ca795b2922516ab53322632b1f5ec81
Tats <js@drone.ws>
2007-09-19 14:32:08 +0000

    * Moved getNativeLibraryExtension() from General.java -> OSUtils.java  * Added new method getJNILibraryExtension()

--- plugins/drone.util/src/drone/util/OSUtils.java
+++ plugins/drone.util/src/drone/util/OSUtils.java
@@ -12,6 +12,40 @@ public final class OSUtils
 	{	super();
 	}
 	
+	/**
+	 * Useful method to get the extension of the native library, depending on current OS.
+	 * @return a library extension
+	 * @throws Exception if OS is unrecognized
+	 */
+	public static String getNativeLibraryExtension() throws Exception {
+		String os = System.getProperty("os.name");
+		if (isMacOSX())
+			return "dylib";
+		else if (isLinux())
+			return "so";
+		else if (isWindows())
+			return "dll";
+		else
+			throw new Exception("Unrecognized OS: " + os);
+	}
+	
+	/**
+	 * Useful method to get the extension of the native library, depending on current OS.
+	 * @return a library extension
+	 * @throws Exception if OS is unrecognized
+	 */
+	public static String getJNILibraryExtension() throws Exception {
+		String os = System.getProperty("os.name");
+		if (isMacOSX())
+			return "jnilib";
+		else if (isLinux())
+			return "so";
+		else if (isWindows())
+			return "dll";
+		else
+			throw new Exception("Unrecognized OS: " + os);
+	}
+	
 	public static final boolean isLinux()
 	{
 		return System.getProperty("os.name").equals("Linux");
