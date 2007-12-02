77f402760d6fe90af876ea7ae6c335de5a36f8c7
Tats <js@drone.ws>
2007-12-02 20:31:12 +0000

    Fixed demo (replaced General by OSUtils)

--- plugins/drone.frei0r/src/drone/frei0r/demo/Frei0rBench.java
+++ plugins/drone.frei0r/src/drone/frei0r/demo/Frei0rBench.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 
 import drone.frei0r.Frei0rException;
 import drone.frei0r.jni.Frei0r;
-import drone.util.General;
+import drone.util.OSUtils;
 
 public class Frei0rBench {
 
@@ -18,7 +18,7 @@ public class Frei0rBench {
 	public static void main(String[] args) throws Exception {
 		int side = (int) Math.sqrt(SIZE);
 		
-		Frei0r frei0r = new Frei0r("/usr/local/lib/frei0r-1/sobel." +  General.getNativeLibraryExtension());
+		Frei0r frei0r = new Frei0r("/usr/local/lib/frei0r-1/sobel." +  OSUtils.getNativeLibraryExtension());
 		Frei0r.Instance inst = frei0r.createInstance(side, side);
 		
 		BufferedImage imgIn = new BufferedImage(side, side, BufferedImage.TYPE_INT_ARGB);
